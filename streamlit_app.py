8422959eca30bc00368ad32ec0330f6032ffcbc6
meenadeviimuralikumar <mdmkumar94@gmail.com>
2024-07-30 21:54:31 +0000

    data exp changes final working code

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,20 +10,37 @@ st.set_page_config(
 
 @st.cache_data
 def load_data(data_path):
-    return pd.read_csv(data_path)
+    df = pd.read_csv(data_path)
+    df = df.rename(columns={"User_Is_Sexist": "User_Rated_Sexism", 
+                            "User_Rated_Sexist_Content_Reasoning": "User_Reasoning"})
+    df = df[['Comment_Id', 'Dataset', 'Comment', 'User_Rated_Sexism', 'User_Reasoning',
+             'Perspective_Toxicity', 'Perspective_Identity_Attack', 'Perspective_Insult','Perspective_Profanity',
+             'GPT_Sexism_Rating','GPT_Sexism_Score']]
+    return df
+
+@st.cache_data
+def getPerspectiveData(df):
+    perspective_df = df.drop(columns = ['GPT_Sexism_Score', 'GPT_Sexism_Rating'])
+    perspective_df.set_index('Comment_Id', inplace=True)
+    return perspective_df
+
+@st.cache_data
+def getGPTData(df):
+    gpt_df = df.drop(columns = ['Perspective_Toxicity', 
+                                'Perspective_Identity_Attack' ,
+                                'Perspective_Insult' ,
+                                'Perspective_Profanity'])
+    gpt_df.set_index('Comment_Id', inplace=True)
+    return gpt_df
+
+
 
 df = load_data("./data/display_sexismdata.csv")
+perspective_df = getPerspectiveData(df)
+gpt_df = getGPTData(df)
 
-def perspective_data():
-    if call_me and (not ben) and (not hos):
-        return df[df['Dataset']=="call_me"].sample(n = rows)
-    elif ben and (not call_me) and (not hos):
-        return df[df['Dataset']=="benevolent"].sample(n = rows)
-    elif hos and (not call_me) and (not ben):
-        return df[df['Dataset']=="hostile"].sample(n = rows)
-    else:
-        return df.sample(n = rows)
-    
+
+
 ###
 ### UI Layout Code Starts
 ###
@@ -32,7 +49,7 @@ st.title("Data Explorer")
 
 sb1 = st.columns(4)
 with sb1[0]:
-    option = st.selectbox(
+    data_option = st.selectbox(
         "Select a dataset",
         ("Sexist_Comment_Dataset", "Other"),
         index = None,
@@ -45,10 +62,10 @@ with sb1[2]:
 with sb1[3]:
     pass
 
-if option == 'Sexist_Comment_Dataset':
+if data_option == 'Sexist_Comment_Dataset':
     sb2 = st.columns(4)
     with sb2[0]:
-        option = st.selectbox(
+        model_option = st.selectbox(
         "Select a model for scoring the data",
         ("Perspective", "GPT"),
         index = None,
@@ -60,33 +77,47 @@ if option == 'Sexist_Comment_Dataset':
     with sb2[3]:
         pass
     st.markdown('######')
+    
     with st.container():
-        if(option == 'Perspective' or option == 'GPT'):
+        if(model_option == 'Perspective' or model_option == 'GPT'):
+            st.divider()
             first = st.columns(3)
             with first[0]:
-                st.write("Filter data by different forms of sexism ")
-                call_me = st.checkbox("Call Me Sexist")
-                ben = st.checkbox("Benevolent Sexism")
-                hos = st.checkbox("Hostile Sexism")
+                st.write("Filter data by different forms of sexism  ")
+                cat_list = df.Dataset.unique()
+                val = [None]* len(cat_list) # this list will store info about which category is selected
+                for i, cat in enumerate(cat_list):
+                    val[i] = st.checkbox(cat, value=True) # value is the preselect value for first render
+                #call_me = st.checkbox("Call Me Sexist")
+                #ben = st.checkbox("Benevolent Sexism")
+                #hos = st.checkbox("Hostile Sexism")
             with first[1]:
+                st.write("Filter data by sexist or non-sexist comments  ")
+                pos = st.radio(
+                    " ",
+                    ["Sexist", "Not Sexist"],
+                )
+                bool_pos =  True if pos == 'Sexist' else False
+            with first[2]:
                 st.write("Select number of rows")
                 rows = st.number_input(" ", 
                                     min_value = 5,
                                     max_value = 25,
                                     step = 5)
-            with first[1]:
-                pass
             st.write(" ")
             html_str = f"""
                 <p><i>Viewing {rows} of 2636 rows</p>
                 """
             st.markdown(html_str, unsafe_allow_html = True)
 
-    if(option == 'Perspective'):
-        st.table(perspective_data())
+    if(model_option == 'Perspective'):
+        st.table(perspective_df[perspective_df.Dataset.isin(cat_list[val]) 
+         & (perspective_df['User_Rated_Sexism'] == bool_pos)].sample(n = rows))
 
+    if(model_option == 'GPT'):
+        st.table(gpt_df[perspective_df.Dataset.isin(cat_list[val]) 
+         & (gpt_df['User_Rated_Sexism'] == bool_pos)].sample(n = rows))
 
-
-if option == 'Other':
+if data_option == 'Other':
     st.write("You can upload datasets using options in the sidebar.")
 
