913c7a888effb0baf553085959665f1c1851f307
meenadeviimuralikumar <mdmkumar94@gmail.com>
2024-08-08 19:46:19 +0000

    commiting final changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,35 +11,82 @@ st.set_page_config(
 @st.cache_data
 def load_data(data_path):
     df = pd.read_csv(data_path)
-    df = df.rename(columns={"User_Is_Sexist": "User_Rated_Sexism", 
-                            "User_Rated_Sexist_Content_Reasoning": "User_Reasoning"})
-    df = df[['Comment_Id', 'Dataset', 'Comment', 'User_Rated_Sexism', 'User_Reasoning',
-             'Perspective_Toxicity', 'Perspective_Identity_Attack', 'Perspective_Insult','Perspective_Profanity',
-             'GPT_Sexism_Rating','GPT_Sexism_Score']]
+    df = df.drop(columns=['Dataset'])
+    df = df[['Comment_Id', 
+             'Community', 
+             'Comment', 
+             'User_Rated_Sexism', 
+             'User_Reasoning',
+             'Perspective_Toxicity',
+             'Perspective_Identity_Attack',
+             'Perspective_Insult',
+             'Perspective_Profanity','GPT_Sexism_Rating','GPT_Sexism_Score']]
     return df
 
 @st.cache_data
-def getPerspectiveData(df):
-    perspective_df = df.drop(columns = ['GPT_Sexism_Score', 'GPT_Sexism_Rating'])
-    perspective_df.set_index('Comment_Id', inplace=True)
-    return perspective_df
+def getPerspectiveToxicityData(df):
+    toxicity_df = df.drop(columns = ['Perspective_Identity_Attack' ,
+                                'Perspective_Insult' ,
+                                'Perspective_Profanity',
+                                'GPT_Sexism_Score', 
+                                'GPT_Sexism_Rating'])
+    toxicity_df.set_index('Comment_Id', inplace=True)
+    return toxicity_df
+
+@st.cache_data
+def getPerspectiveIDAData(df):
+    ida_df = df.drop(columns = ['Perspective_Toxicity' ,
+                                'Perspective_Insult' ,
+                                'Perspective_Profanity',
+                                'GPT_Sexism_Score', 
+                                'GPT_Sexism_Rating'])
+    ida_df.set_index('Comment_Id', inplace=True)
+    return ida_df
+
+@st.cache_data
+def getPerspectiveInsultData(df):
+    insult_df = df.drop(columns = ['Perspective_Toxicity' ,
+                                'Perspective_Identity_Attack' ,
+                                'Perspective_Profanity',
+                                'GPT_Sexism_Score', 
+                                'GPT_Sexism_Rating'])
+    insult_df.set_index('Comment_Id', inplace=True)
+    return insult_df
+
+@st.cache_data
+def getPerspectiveProfanityData(df):
+    profanity_df = df.drop(columns = ['Perspective_Toxicity' ,
+                                'Perspective_Identity_Attack' ,
+                                'Perspective_Insult',
+                                'GPT_Sexism_Score', 
+                                'GPT_Sexism_Rating'])
+    profanity_df.set_index('Comment_Id', inplace=True)
+    return profanity_df
 
 @st.cache_data
 def getGPTData(df):
     gpt_df = df.drop(columns = ['Perspective_Toxicity', 
                                 'Perspective_Identity_Attack' ,
                                 'Perspective_Insult' ,
-                                'Perspective_Profanity'])
+                                'Perspective_Profanity',
+                                'GPT_Sexism_Rating'])
     gpt_df.set_index('Comment_Id', inplace=True)
     return gpt_df
 
 
-
-df = load_data("./data/display_sexismdata.csv")
-perspective_df = getPerspectiveData(df)
+df = load_data("./data/sexism_prototype.csv")
+toxicity_df = getPerspectiveToxicityData(df)
+ida_df = getPerspectiveIDAData(df)
+insult_df = getPerspectiveInsultData(df)
+profanity_df = getPerspectiveProfanityData(df)
 gpt_df = getGPTData(df)
 
-
+model_attributes_list = ["Perspective Toxicity", 
+         "Perspective Identity Attack",
+         "Perspective Insult",
+         "Perspective Profanity",
+         "GPT Sexism"]
+rows = 5
 
 ###
 ### UI Layout Code Starts
@@ -66,8 +113,12 @@ if data_option == 'Sexist_Comment_Dataset':
     sb2 = st.columns(4)
     with sb2[0]:
         model_option = st.selectbox(
-        "Select a model for scoring the data",
-        ("Perspective", "GPT"),
+        "Select a model and attribute to view scores",
+        ("Perspective Toxicity", 
+         "Perspective Identity Attack",
+         "Perspective Insult",
+         "Perspective Profanity",
+         "GPT Sexism"),
         index = None,
         placeholder=" ",)
     with sb2[1]:
@@ -79,18 +130,15 @@ if data_option == 'Sexist_Comment_Dataset':
     st.markdown('######')
     
     with st.container():
-        if(model_option == 'Perspective' or model_option == 'GPT'):
+        if(model_option in model_attributes_list):
             st.divider()
             first = st.columns(3)
             with first[0]:
-                st.write("Filter data by different forms of sexism  ")
-                cat_list = df.Dataset.unique()
+                st.write("Filter data by the Community from which the comment was sourced.")
+                cat_list = df.Community.unique()
                 val = [None]* len(cat_list) # this list will store info about which category is selected
                 for i, cat in enumerate(cat_list):
                     val[i] = st.checkbox(cat, value=True) # value is the preselect value for first render
-                #call_me = st.checkbox("Call Me Sexist")
-                #ben = st.checkbox("Benevolent Sexism")
-                #hos = st.checkbox("Hostile Sexism")
             with first[1]:
                 st.write("Filter data by sexist or non-sexist comments  ")
                 pos = st.radio(
@@ -110,13 +158,26 @@ if data_option == 'Sexist_Comment_Dataset':
                 """
             st.markdown(html_str, unsafe_allow_html = True)
 
-    if(model_option == 'Perspective'):
-        st.table(perspective_df[perspective_df.Dataset.isin(cat_list[val]) 
-         & (perspective_df['User_Rated_Sexism'] == bool_pos)].sample(n = rows))
+    if(model_option == model_attributes_list[0]):
+        st.table(toxicity_df[toxicity_df.Community.isin(cat_list[val]) 
+         & (toxicity_df['User_Rated_Sexism'] == bool_pos)].sample(n = rows))
 
-    if(model_option == 'GPT'):
-        st.table(gpt_df[perspective_df.Dataset.isin(cat_list[val]) 
+    if(model_option == model_attributes_list[1]):
+        st.table(ida_df[ida_df.Community.isin(cat_list[val]) 
+         & (ida_df['User_Rated_Sexism'] == bool_pos)].sample(n = rows))
+        
+    if(model_option == model_attributes_list[2]):
+        st.table(insult_df[insult_df.Community.isin(cat_list[val]) 
+         & (insult_df['User_Rated_Sexism'] == bool_pos)].sample(n = rows))
+        
+    if(model_option == model_attributes_list[3]):
+        st.table(profanity_df[profanity_df.Community.isin(cat_list[val]) 
+         & (profanity_df['User_Rated_Sexism'] == bool_pos)].sample(n = rows))
+        
+    if(model_option == model_attributes_list[4]):
+        st.table(gpt_df[gpt_df.Community.isin(cat_list[val]) 
          & (gpt_df['User_Rated_Sexism'] == bool_pos)].sample(n = rows))
+
 
 if data_option == 'Other':
     st.write("You can upload datasets using options in the sidebar.")
